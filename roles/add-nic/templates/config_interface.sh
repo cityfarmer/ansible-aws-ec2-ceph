4e6991fb5b675b11ce3d47175e4c8de378d59dfd
cityfarmer <system@gmail.com>
2018-11-01 03:55:48 +0000

    fixed add nic script

--- roles/add-nic/templates/config_interface.sh
+++ roles/add-nic/templates/config_interface.sh
@@ -2,7 +2,8 @@
 #set -x
 . ~/.bash_profile
 
-privip=`aws ec2 describe-network-interfaces --filters Name=description,Values=Eagle-Ceph-Cluster-Interface | grep PrivateIpAddress\" | tail -1 | awk -F':' '{print $2}' | sed -e 's/[\" ]//g'`
+inst=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`
+privip=`aws ec2 describe-network-interfaces --filters Name=description,Values=Eagle-Ceph-Cluster-Interface Name=attachment.instance-id,Values=$inst | grep PrivateIpAddress\" | tail -1 | awk -F':' '{print $2}' | sed -e 's/[\" ]//g'`
 nmcli con add type ethernet con-name eth1 ifname eth1 ip4 ${privip}/20
 
 exit 0
